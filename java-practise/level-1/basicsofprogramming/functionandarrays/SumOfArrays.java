aa9ce090fbf29192df8e6ada1de7944bc505be5a
airbornesd <dhaka.siddharth1999@gmail.com>
2021-08-02 22:18:39 +0530

    Sum of Two Arrays

--- java-practise/level-1/basicsofprogramming/functionandarrays/SumOfArrays.java
+++ java-practise/level-1/basicsofprogramming/functionandarrays/SumOfArrays.java
@@ -8,19 +8,26 @@ public class SumOfArrays {
         // n1 size of Array, a1 elements of Arrays
         int n1 = scn.nextInt();
         int[] a1 = new int[n1];
-        for (int i = 0; i < n1; i++){
+        for (int i = 0; i < n1; i++) {
             a1[i] = scn.nextInt();
         }
 
         // n2 size of Array, a2 elements of Arrays
         int n2 = scn.nextInt();
         int[] a2 = new int[n2];
-        for (int i = 0; i < n2; i++){
+        for (int i = 0; i < n2; i++) {
             a2[i] = scn.nextInt();
         }
 
-        // Adding both array
+        int lengthOfa3 = 0;
+        if (a1.length > a2.length) {
+            lengthOfa3 = a1.length + 1;
+        } else {
+            lengthOfa3 = a2.length + 1;
+        }
+        int[] a3 = new int[lengthOfa3];
 
+        for (int )
 
     }
 }
