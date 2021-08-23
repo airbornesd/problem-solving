e11a658c890c506dae94ea09666c329318d1a6ad
airbornesd <dhaka.siddharth1999@gmail.com>
2021-08-23 23:19:29 +0530

    arrays

---
+++ java-practise/level-1/basicsofprogramming/functionandarrays/BrokenEconomy.java
@@ -0,0 +1,31 @@
+package basicsofprogramming.functionandarrays;
+
+import java.util.Scanner;
+
+public class BrokenEconomy {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt(); // no of element in the array
+        int[] a = new int[n];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = scn.nextInt();
+        }
+        int d = scn.nextInt();
+        int count = a.length;
+        int temp = 0;
+        for (int i = a.length - 1; i >= 0; i--) {
+
+            if (a[i] < d) {
+                temp = a[i];
+                break;
+            }
+            count--;
+        }
+        if (a[count] == d) {
+            System.out.println(a[count]);
+        } else {
+            System.out.println(a[count]);
+            System.out.println(temp);
+        }
+    }
+}
--- java-practise/level-1/basicsofprogramming/functionandarrays/FindElementInAnArray.java
+++ java-practise/level-1/basicsofprogramming/functionandarrays/FindElementInAnArray.java
@@ -25,3 +25,5 @@ public class FindElementInAnArray {
 
     }
 }
+
+// linear search
