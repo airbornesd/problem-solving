8e07979add302f8d622a8612c6a08f4f6b73f529
airbornesd <dhaka.siddharth1999@gmail.com>
2021-08-28 22:29:58 +0530

    array update

---
+++ java-practise/level-1/basicsofprogramming/functionandarrays/FirstIndexAndLastIndex.java
@@ -0,0 +1,9 @@
+package basicsofprogramming.functionandarrays;
+
+import java.util.Scanner;
+
+public class FirstIndexAndLastIndex {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+    }
+}
