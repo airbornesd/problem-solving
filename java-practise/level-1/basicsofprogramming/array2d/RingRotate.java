3454e9cb81927991fe629731e987045a91a07149
airbornesd <dhaka.siddharth1999@gmail.com>
2021-09-01 23:45:09 +0530

    2d array

--- java-practise/level-1/basicsofprogramming/array2d/RingRotate.java
+++ java-practise/level-1/basicsofprogramming/array2d/RingRotate.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 
 public class RingRotate {
     public static void main(String[] args) throws Exception {
+        //taking input
         Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        int m = scn.nextInt();
+        int n = scn.nextInt(); //number of rows
+        int m = scn.nextInt(); //number of columns
         int[][] arr = new int[n][m];
 
         for (int i = 0; i < n; i++) {
@@ -14,46 +15,111 @@ public class RingRotate {
                 arr[i][j] = scn.nextInt();
             }
         }
-        int s = scn.nextInt();
-        int r = scn.nextInt();
+        int s = scn.nextInt(); //ring number
+        int r = scn.nextInt(); //number of rotations
 
-        int rMin = arr[(s - 1)][(s - 1)], cMin = arr[(n - s)][(s - 1)], rMax = arr[(s - 1)][(m - s)], cMax = arr[(n - s)][(m - s)];
-        System.out.println(rMin + " " + cMin + " " + rMax + " " + cMax);
-        System.out.println();
+        ringRotate(arr, s, r);
+        display(arr);
+    }
+
+    public static void ringRotate(int[][] arr, int s, int r) {
+
+        int[] la = fill1Dfrom2D(arr, s);
+        rotate(la, r);
+        fill2Dfrom1D(arr, la, s);
+
+    }
+
+    public static int[] fill1Dfrom2D(int[][] arr, int s) {
+        int rMin = s - 1, cMin = s - 1, rMax = arr.length - s, cMax = arr[0].length - s;
+
+        int size = 2 * (cMax - cMin + rMax - rMin);
+        int[] arr2 = new int[size];
         int count = 0;
-        int[] arr2 = new int[m * n];
-        while (count < n * m) {
-            //left boundary
-            for (int row = rMin; row <= rMax; row++) {
-                System.out.println(row);
-                //arr2[count] = arr[row][cMin];
-                //System.out.println(arr[row][cMin]);
-                count++;
-            }
 
-            //bottom boundary
-            for (int col = cMin; col <= cMax; col++) {
-                System.out.println(col);
-                //arr2[count] = arr[rMax][col];
-                //System.out.println(arr[rMax][col]);
-                count++;
-            }
+        //left boundary
+        for (int row = rMin; row <= rMax; row++) {
+            arr2[count] = arr[row][cMin];
+            count++;
+        }
+        cMin++;
 
-            //right boundary
-            for (int row = rMax; row >= rMin; row--) {
-                System.out.println(row);
-                //arr2[count] = arr[row][cMax];
-                //System.out.println(arr[row][cMax]);
-                count++;
-            }
+        //bottom boundary
+        for (int col = cMin; col <= cMax; col++) {
+            arr2[count] = arr[rMax][col];
+            count++;
+        }
+        rMax--;
 
-            //top boundary
-            for (int col = cMax; col >= cMin; col--) {
-                System.out.println(col + " hehe");
-                //arr2[count] = arr[rMin][col];
-                //System.out.println(arr[rMin][col]);
-                count++;
-            }
+        //right boundary
+        for (int row = rMax; row >= rMin; row--) {
+            arr2[count] = arr[row][cMax];
+            count++;
+        }
+        cMax--;
+
+        //top boundary
+        for (int col = cMax; col >= cMin; col--) {
+            arr2[count] = arr[rMin][col];
+            count++;
+        }
+
+        return arr2;
+    }
+
+    public static void rotate(int[] a, int k) {
+        k = k % a.length;
+        if (k < 0) {
+            k = k + a.length;
+        }
+
+        int[] ans1 = reverse(a, 0, a.length - 1);
+        ans1 = reverse(ans1, 0, k - 1);
+        ans1 = reverse(ans1, k, a.length - 1);
+
+    }
+
+    public static int[] reverse(int[] a, int left, int right) {
+        while (left < right) {
+            int temp = a[left];
+            a[left] = a[right];
+            a[right] = temp;
+            left++;
+            right--;
+        }
+        return a;
+    }
+
+    public static void fill2Dfrom1D(int[][] arr, int[] la, int s) {
+        int rMin = s - 1, cMin = s - 1, rMax = arr.length - s, cMax = arr[0].length - s;
+
+        int count = 0;
+
+        //left boundary
+        for (int row = rMin; row <= rMax; row++) {
+            arr[row][cMin] = la[count] ;
+            count++;
+        }
+        cMin++;
+
+        //bottom boundary
+        for (int col = cMin; col <= cMax; col++) {
+            arr[rMax][col] = la[count] ;
+            count++;
+        }
+        rMax--;
+
+        //right boundary
+        for (int row = rMax; row >= rMin; row--) {
+            arr[row][cMax] = la[count] ;
+            count++;
+        }
+        cMax--;
+
+        //top boundary
+        for (int col = cMax; col >= cMin; col--) {
+            arr[rMin][col] = la[count] ;
+            count++;
         }
 
     }
