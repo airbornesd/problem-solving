4a77d43610b194659deeedb5f965b59a1bbf72e4
airbornesd <dhaka.siddharth1999@gmail.com>
2021-09-07 19:10:52 +0530

    string intro

---
+++ java-practise/level-1/basicsofprogramming/string/PrintAllPalindromicSubstrings.java
@@ -0,0 +1,17 @@
+package basicsofprogramming.string;
+
+import java.util.Scanner;
+
+public class PrintAllPalindromicSubstrings {
+
+    public static void solution(String str) {
+        //write your code here
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        solution(str);
+    }
+}
