51fc92a200f85bb36638c726ccc0fe4bbae3a404
airbornesd <dhaka.siddharth1999@gmail.com>
2021-09-13 23:52:28 +0530

    update

--- java-practise/level-1/basicsofprogramming/patterns/Pattern7.java
+++ java-practise/level-1/basicsofprogramming/patterns/Pattern7.java
@@ -7,7 +7,7 @@ public class Pattern7 {
         Scanner scn = new Scanner( System.in);
         int n = scn.nextInt();
         for (int i = 1; i <= n; i++){
-            for (int j = 0; j <= i; j++){
+            for (int j = 1; j <= i; j++){
                 if (i == j) {
                     System.out.print("*\t");
                 } else {
